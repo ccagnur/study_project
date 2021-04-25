7a6286a3f34db3ad5970d9bbb7343db1c77310cb
cagnur k <chagnur.kaya@gmail.com>
2021-04-25 15:15:33 +0300

    Regression Analysis of US Judge Rates

---
+++ judge_rates.R
@@ -0,0 +1,17 @@
+pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes, ggvis, httr, 
+               lubridate, plotly, rio, rmarkdown, shiny, stringr, tidyr)
+library(datasets)
+
+head(USJudgeRatings)
+data <- USJudgeRatings
+?USJudgeRatings
+
+x <- as.matrix(data[-12])
+y <- data[, 12]
+
+reg1 <- lm(y ~ x)
+reg1
+summary(reg1)
+
+anova(reg1)
+coef(reg1)
